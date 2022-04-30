5ba44c59f57a6503eb64e25135a68ff827da1b2b
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2022-04-30 14:31:09 -0300

    Atualizado verbose_name model Sale

--- crud_sales/core/models.py
+++ crud_sales/core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 class Sale(models.Model):
     sale_id = models.IntegerField()
-    created_at = models.DateTimeField()
-    total = models.FloatField()
-    status = models.CharField(max_length=50)
-    products_count = models.IntegerField()
-    state = models.CharField(max_length=50)
+    created_at = models.DateTimeField(verbose_name='Data de Criacao')
+    total = models.FloatField(verbose_name="Total venda")
+    status = models.CharField(max_length=50, verbose_name="Status Venda")
+    products_count = models.IntegerField(verbose_name='Quantidade de produtos')
+    state = models.CharField(max_length=50, verbose_name='Estado')
 
     class Meta:
+        verbose_name = 'Venda'
+        verbose_name_plural = 'Vendas'
         db_table = "sales"
