c55c4f1336f3f05bee22a9bdbe1aeeb47e1ae769
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2022-04-30 14:32:18 -0300

    Adicionad model Sale ao painel admin

--- crud_sales/core/admin.py
+++ crud_sales/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from crud_sales.core.models import Sale
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('sale_id', 'created_at', 'total', 'status', 'products_count', 'state',)
+    search_fields = ('sale_id',)
+    list_filter = ('created_at',)
